d3edea4689047b9f57f406afdd2f65aeabca038a
Vajrapani <incompletestoic@gmail.com>
2019-10-31 12:28:43 +0000

    Slightly more clean player movement

--- classes.lua
+++ classes.lua
@@ -3,15 +3,87 @@ require 'mapAndLighting'
 require 'miscSettings'
 
 actor = class('actor') -- actor class
+id = 0
 
 function actor:initialize(x, y, viewRadius) -- all actors will have (x,y) co-ordinates
 self.x = x * gridMultiplier
 self.y = y * gridMultiplier
-self.viewRadius = 8
+self.viewRadius = 8 -- 8, usually
+self.energy = 0
+self.energyThreshold = 1 -- increment -> do action -> decrement -> next actor
+self.input = nil
+self.id = id
+
+-- Add new actor to table of actors
+    table.insert(actors, self)
+    id = id + 1
+
 end
 
 player = class('player', actor) -- player subclass
 
 function player:initialize(x, y)
-actor.initialize(self, x, y, 8)
+actor.initialize(self, x, y)
 end
+
+function player:takeAction()
+self.energy = self.energy + 1
+
+if self.energy >= self.energyThreshold then
+self:getDecision()
+self.energy = self.energy - 1
+end
+end -- takeAction()
+
+function player:getDecision()
+  if self.input == "kp1" then -- lower left diagonal
+      if testMap(-1, 1) == true then
+      Warlock.x = Warlock.x - gridMultiplier
+      Warlock.y = Warlock.y + gridMultiplier
+      end
+
+  elseif self.input == "kp2" then -- go down
+    if testMap(0, 1) == true then
+    Warlock.y = Warlock.y + gridMultiplier
+    end
+
+  elseif self.input == "kp3" then -- lower right diagonal
+    if testMap(1, 1) == true then
+    Warlock.x = Warlock.x + gridMultiplier
+    Warlock.y = Warlock.y + gridMultiplier
+    end
+
+  elseif self.input == "kp4" then -- left
+    if testMap(-1, 0) == true then
+    Warlock.x = Warlock.x - gridMultiplier
+    end
+
+  elseif self.input == "kp5" then -- stay in place, pass a turn
+    if testMap(0, 0) == true then
+    end
+
+  elseif self.input == "kp6" then -- right
+    if testMap(1, 0) == true then
+    Warlock.x = Warlock.x + gridMultiplier
+  end
+
+  elseif self.input == "kp7" then -- upper left diagonal
+    if testMap(-1, -1) == true then
+    Warlock.x = Warlock.x - gridMultiplier
+    Warlock.y = Warlock.y - gridMultiplier
+  end
+
+  elseif self.input == "kp8" then -- up
+    if testMap(0, -1) == true then
+    Warlock.y = Warlock.y - gridMultiplier
+  end
+
+  elseif self.input == "kp9" then -- upper right diagonal
+    if testMap(1, -1) == true then
+    Warlock.x = Warlock.x + gridMultiplier
+    Warlock.y = Warlock.y - gridMultiplier
+  end
+
+  end -- end of big if statement with the numpad buttons
+
+  end -- getDecision()
--- main.lua
+++ main.lua
@@ -3,11 +3,11 @@ require 'miscSettings' -- misc values and functions that are useful
 require 'mapAndLighting' -- anything to do with the map and lighting/FOV
 require 'classes' -- all classes
 
+actors = {} -- table gets populated by the actor class as new ones are created
+
 function love.load()
-actors = {} -- first actor should always be the player (Warlock)
 drunkWalk() -- Generates a cave-like map by using the drunk walk algorithm
 Warlock = player:new(Place()) -- Warlock is the player char
-table.insert(actors, Warlock) -- so actor[1] = {x = something, y = something , viewRadius = 8}
 end
 
 function love.update(dt)
@@ -25,63 +25,8 @@ end
 
 
 function love.keypressed(key)
-  if key == "8" or key == "kp8" then  -- go up
-  if testMap(0, -1) == true then
-  Warlock.y = Warlock.y - gridMultiplier
+if key == "kp1" or "kp2" or "kp3" or "kp4" or "kp5" or "kp6" or "kp7" or "kp8" or "kp9" then -- only numpad
+  Warlock.input = key
+  Warlock:takeAction()
   end
-
-
-elseif key == "4" or key == "kp4" then -- go left
-
-  if testMap(-1, 0) == true then
-  Warlock.x = Warlock.x - gridMultiplier
-  end
-
-elseif key == "5" or key == "kp5" then -- stay in place
-
-  if testMap(0, 0) == true then
-  --Warlock.x = Warlock.x - gridMultiplier
-  end
-
-elseif key == "7" or key == "kp7" then -- upper left diagonal
-
-  if testMap(-1, -1) == true then
-  Warlock.x = Warlock.x - gridMultiplier
-  Warlock.y = Warlock.y - gridMultiplier
-  end
-
-elseif key == "2" or key == "kp2" then -- go down
-
-  if testMap(0, 1) == true then
-  Warlock.y = Warlock.y + gridMultiplier
-  end
-
-elseif key == "6" or key == "kp6" then -- go right
-
-  if testMap(1, 0) == true then
-  Warlock.x = Warlock.x + gridMultiplier
-  end
-
-elseif key == "9" or key == "kp9" then -- upper right diagonal
-
-  if testMap(1, -1) == true then
-  Warlock.x = Warlock.x + gridMultiplier
-  Warlock.y = Warlock.y - gridMultiplier
-  end
-
-elseif key == "1" or key == "kp1" then -- lower left diagonal
-
-  if testMap(-1, 1) == true then
-  Warlock.x = Warlock.x - gridMultiplier
-  Warlock.y = Warlock.y + gridMultiplier
-  end
-
-elseif key == "3" or key == "kp3" then -- lower right diagonal
-
-  if testMap(1, 1) == true then
-  Warlock.x = Warlock.x + gridMultiplier
-  Warlock.y = Warlock.y + gridMultiplier
- end
-
-end
 end
