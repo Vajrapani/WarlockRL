f10a7ff89d15abc48617dd4f7d4b9bb61bcf53bd
Vajrapani <incompletestoic@gmail.com>
2019-11-02 05:31:55 +0000

    better flavour

--- gameText.lua
+++ gameText.lua
@@ -1,2 +1,2 @@
-OpeningLines = [[You have made a blood pact with a demonic patron. Harvest the wretched souls that
-slither beneath the ground here to prove yourself worthy and be rewarded with power beyond your wildest dreams!]]
+OpeningLines = [[You have made a blood pact with a demonic entity. Harvest the souls that
+slither beneath the ground here to prove your worth and, in turn,  be rewarded with unimaginable power.]]
