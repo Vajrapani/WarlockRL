90f968a3801bc88d23fb8594bbf741794fcae8aa
Vajrapani <incompletestoic@gmail.com>
2019-11-02 03:40:32 +0000

    make stuff prettier

--- miscSettingsAndFunctions.lua
+++ miscSettingsAndFunctions.lua
@@ -40,9 +40,9 @@ end --positionIsOpenFunc()
 
 function UserInterface()
   love.graphics.setFont(InconsolataRegular)
-  love.graphics.print({{NormaliseRGB(203, 75, 22)}, "[ W A R L O C K ]"}, 37 * gridMultiplier, 1 * gridMultiplier)
-  love.graphics.print({{NormaliseRGB(203, 75, 22)}, "[ Turncount ] : " .. turncount}, 37 * gridMultiplier, 5 * gridMultiplier)
-  love.graphics.print({{NormaliseRGB(203, 75, 22)}, "[ Health ] : " .. Warlock.health}, 37 * gridMultiplier, 3 * gridMultiplier)
+  love.graphics.print({{NormaliseRGB(203, 75, 22)}, "* W A R L O C K *"}, 37 * gridMultiplier, 1 * gridMultiplier)
+  love.graphics.print({{NormaliseRGB(203, 75, 22)}, "* Turncount : " .. turncount}, 37 * gridMultiplier, 5 * gridMultiplier)
+  love.graphics.print({{NormaliseRGB(203, 75, 22)}, "* Health : " .. Warlock.health}, 37 * gridMultiplier, 3 * gridMultiplier)
   --love.graphics.print({{NormaliseRGB(203, 75, 22)}, "[ Monster Health ] : " .. monster1.health}, 37 * gridMultiplier, 7 * gridMultiplier)
 end
 
