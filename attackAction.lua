13082d82d73b3ce8bdf8a6ad1ed607879dfcda8a
Vajrapani <incompletestoic@gmail.com>
2019-11-05 19:59:31 +0000

    better attack message

--- attackAction.lua
+++ attackAction.lua
@@ -21,8 +21,7 @@ function AttackAction:attack(actor, damage, x, y)
   actors[actor].health = actors[actor].health - Warlock.damage
   Warlock.isInCombat = true
   Warlock.soulPower = Warlock.soulPower + 1
-  MessageBox:insertIntoLog("You empower yourself with the " .. actors[actor].name .. "'s lifeforce.")
-  MessageBox:insertIntoLog("You strike the " .. actors[actor].name .. "!")
+  MessageBox:insertIntoLog("You strike the " .. actors[actor].name .. " and empower yourself with its Lifeforce.")
   if actors[actor].health <= 0 then
     MessageBox:insertIntoLog("You kill the " .. actors[actor].name .. " and capture its soul in a flask.")
     table.remove(actors, actor) ; actorMap[x / gridMultiplier][y / gridMultiplier] = ""
