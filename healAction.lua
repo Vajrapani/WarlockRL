17f19887e460917f80cfdb3018cbd0fdf8659885
Vajrapani <incompletestoic@gmail.com>
2019-11-05 12:47:13 +0000

    Soul flasks heal for 10-20 hp now

--- healAction.lua
+++ healAction.lua
@@ -9,7 +9,7 @@ end
 
 function HealAction:playerHeal()
 if Warlock.healthFlasks >= 1 then
-  Warlock.health = Warlock.health + love.math.random(5, 10)
+  Warlock.health = Warlock.health + love.math.random(10, 20)
   if Warlock.health > Warlock.maxHealth then Warlock.health = Warlock.maxHealth end
   Warlock.healthFlasks = Warlock.healthFlasks - 1
 end
