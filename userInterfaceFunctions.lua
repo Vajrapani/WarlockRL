0740c7271d9a8d07cf79a0b79a006f729ad838e4
Vajrapani <incompletestoic@gmail.com>
2019-11-07 18:09:06 +0000

    'Soul Power' to 'Soul Fragments' in the UI

--- userInterfaceFunctions.lua
+++ userInterfaceFunctions.lua
@@ -3,7 +3,7 @@ function UserInterface()
   love.graphics.print({{NormaliseRGB(203, 75, 22)}, "* W A R L O C K *"}, 37 * gridMultiplier, 1 * gridMultiplier)
   love.graphics.print({{NormaliseRGB(203, 75, 22)}, "* Health : " .. Warlock.health .. " / " .. Warlock.maxHealth}, 37 * gridMultiplier, 3 * gridMultiplier)
   love.graphics.print({{NormaliseRGB(203, 75, 22)}, "* Turncount : " .. turncount}, 37 * gridMultiplier, 5 * gridMultiplier)
-  love.graphics.print({{NormaliseRGB(203, 75, 22)}, "* Soul Power : " .. Warlock.soulPower}, 37 * gridMultiplier, 7 * gridMultiplier)
+  love.graphics.print({{NormaliseRGB(203, 75, 22)}, "* Soul Fragments : " .. Warlock.soulPower}, 37 * gridMultiplier, 7 * gridMultiplier)
   love.graphics.print({{NormaliseRGB(203, 75, 22)}, "* In-hand : " ..  "+"  .. (Warlock.damage - Warlock.baseDamage) .. " " .. Weapon.name}, 37 * gridMultiplier, 11 * gridMultiplier)
   love.graphics.print({{NormaliseRGB(203, 75, 22)}, "* Soul Flasks : " .. Warlock.healthFlasks}, 37 * gridMultiplier, 13 * gridMultiplier)
 end
